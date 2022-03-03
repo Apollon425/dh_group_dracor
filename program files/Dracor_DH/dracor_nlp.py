c290313af2ecc6b7d3307a1ed861da6d64e12100
phelotom <61209836+phelotom@users.noreply.github.com>
2022-03-03 17:01:01 +0100

    Added, code for POS-Tagging and Lemmatization
    is imported in the new dracor_data

---
+++ program files/Dracor_DH/dracor_nlp.py
@@ -0,0 +1,24 @@
+import spacy
+
+class Preprocessor:
+    def __init__(self, texts, lang):
+        self.spacy_docs = []
+        if lang=="ita":
+            nlp = spacy.load("it_core_news_lg", disable=['ner'])
+        else:
+            nlp = spacy.load("de_dep_news_trf")
+        for text in texts:
+            self.spacy_docs.append(nlp(text))
+
+    def __call__(self):     # __call__ and not a normal function name because tfidf-vectorizer demands a callable
+        lemmatized_texts = []
+        for doc in self.spacy_docs:
+            lemmatized_texts.append(" ".join([token.lemma_ for token in doc]))
+        return lemmatized_texts
+
+    def pos_tag(self):
+        pos_tag_features = []
+        for doc in self.spacy_docs:
+            # text_pos = [token.pos_ for token in doc]    # POS bigrams?
+            pos_tag_features.append(doc.count_by(spacy.attrs.POS))  # convert to panda df?
+        return pos_tag_features
